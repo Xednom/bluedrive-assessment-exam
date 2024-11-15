5830c909a42fcd55ef4ee0f4aa4e145fcb76a377
Xednom <monde.lacanlalay@gmail.com>
2024-11-15 18:43:14 +0800

    added Post Detail api endpoint also included with list of comments

--- apps/api/resources/post.py
+++ apps/api/resources/post.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers, permissions, status
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
 
-from apps.api.models import Post
+from apps.api.models import Post, Comment
 
 
 class PostFilter(filters.FilterSet):
@@ -17,12 +17,28 @@ class PostFilter(filters.FilterSet):
         fields = ["title", "author_name", "published_date"]
 
 
+class PostCommentSerializer(serializers.ModelSerializer):
+    commenter = serializers.CharField(source="post.author.name", read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["commenter", "content", "created_at"]
+
+
 class PostSerializer(serializers.ModelSerializer):
     author_name = serializers.CharField(source="author.name", read_only=True)
+    comment_set = PostCommentSerializer(many=True)
 
     class Meta:
         model = Post
-        fields = ["id", "title", "content", "published_date", "author_name"]
+        fields = [
+            "id",
+            "title",
+            "content",
+            "published_date",
+            "author_name",
+            "comment_set",
+        ]
 
 
 class PostAPIView(ListAPIView):
@@ -30,3 +46,9 @@ class PostAPIView(ListAPIView):
     serializer_class = PostSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = PostFilter
+
+
+class PostDetailView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = "id"
--- apps/api/router.py
+++ apps/api/router.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from apps.api.resources.post import PostAPIView
+from apps.api.resources.post import PostAPIView, PostDetailView
 
 
 router = routers.DefaultRouter()
@@ -9,4 +9,5 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path("v1/", include(router.urls), name="api"),
     path("post/", PostAPIView.as_view(), name="post_list"),
+    path("post/<int:id>/", PostDetailView.as_view(), name="post_detail"),
 ]
